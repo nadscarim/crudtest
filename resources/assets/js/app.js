24f8cc8fe3b4bc767860ac812a45c13c7f17b157
jcmlumacad1 <jcmlumacad@tmj.ph>
2019-03-20 09:36:03 +0800

    added material ui

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import AppBar from "./shared/components/AppBar";
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        hello world
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <AppBar />
+                hello world
+            </div>
+        )
+    }
 }
+
 
 if (document.getElementById('root')) {
     ReactDOM.render(<App />, document.getElementById('root'));
