45daf888d6663300486e9af999dd058855b6813e
jcmlumacad1 <jcmlumacad@tmj.ph>
2019-03-20 10:51:26 +0800

    registered commands

--- start/app.js
+++ start/app.js
@@ -11,14 +11,14 @@
 |
 */
 const providers = [
-  '@adonisjs/framework/providers/AppProvider',
-  '@adonisjs/framework/providers/ViewProvider',
-  '@adonisjs/lucid/providers/LucidProvider',
-  '@adonisjs/bodyparser/providers/BodyParserProvider',
-  '@adonisjs/cors/providers/CorsProvider',
-  '@adonisjs/shield/providers/ShieldProvider',
-  '@adonisjs/session/providers/SessionProvider',
-  '@adonisjs/auth/providers/AuthProvider'
+    '@adonisjs/framework/providers/AppProvider',
+    '@adonisjs/framework/providers/ViewProvider',
+    '@adonisjs/lucid/providers/LucidProvider',
+    '@adonisjs/bodyparser/providers/BodyParserProvider',
+    '@adonisjs/cors/providers/CorsProvider',
+    '@adonisjs/shield/providers/ShieldProvider',
+    '@adonisjs/session/providers/SessionProvider',
+    '@adonisjs/auth/providers/AuthProvider'
 ]
 
 /*
@@ -31,7 +31,7 @@ const providers = [
 |
 */
 const aceProviders = [
-  '@adonisjs/lucid/providers/MigrationsProvider'
+    '@adonisjs/lucid/providers/MigrationsProvider'
 ]
 
 /*
@@ -56,6 +56,16 @@ const aliases = {}
 | Here you store ace commands for your package
 |
 */
-const commands = []
+const commands = [
+    'App/Commands/TmjLocale',
+    'App/Commands/TmjSeed',
+    'App/Commands/TmjDropCreateMigrate',
+    'App/Commands/TmjMigration/TmjMigrationUp',
+    'App/Commands/TmjMigration/TmjMigrationRollBack',
+    'App/Commands/TmjMigration/TmjMigrationMake',
+    'App/Commands/TmjMigration/TmjMigrationRunAll',
+    'App/Commands/TmjMigration/TmjMigrationRollBackAll',
+    'App/Commands/TmjMigration/TmjMigrationRefresh'
+]
 
 module.exports = { providers, aceProviders, aliases, commands }
