e5cb437ec47e6f979ae14cfda91ee94360659ca7
jcmlumacad1 <jcmlumacad@tmj.ph>
2019-03-21 12:07:50 +0800

    added config controller

---
+++ app/Controllers/Http/Foobar/ConfigController.js
@@ -0,0 +1,39 @@
+"use strict"
+
+const Config = use("Config")
+
+class ConfigController {
+    async index({ response }) {
+        /**
+         * NOTE: Expose config is a security risk be sure to only expose data
+         * that is not confidential e.g.(password, encryption key etc)
+         * DO NOT EXPOSE EVERYTHING. select the things you only require in the client
+         */
+        let configData = {
+            shared: {
+                general: this._getGeneralConfig()
+            },
+            foobar: this._getFoobarConfig()
+        }
+
+        let config = "config = " + JSON.stringify(configData) + ";"
+        response.header("Content-type", "text/javascript")
+        response.send(config)
+    }
+
+    _getGeneralConfig() {
+        return {
+            company: Config.get("shared.general.company"),
+        }
+    }
+
+    _getFoobarConfig() {
+        let foobar = Config.get("foobar")
+
+        return {
+            foobar: foobar.foobar
+        }
+    }
+}
+
+module.exports = ConfigController
