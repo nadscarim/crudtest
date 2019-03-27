27683279ce07f94f6f0549474e8ad55b6643b253
jcmlumacad1 <jcmlumacad@tmj.ph>
2019-03-27 10:26:56 +0800

    added sample usage for react loadable & hashrouter

--- resources/assets/js/foobar/FoobarContainer.js
+++ resources/assets/js/foobar/FoobarContainer.js
@@ -3,6 +3,7 @@ import TmjPrompt from '../shared/TmjPrompt'
 import axios from '../shared/bootstrap/axios.config'
 import SampleSocketIO from '../sample/sample-socketio/SampleSocketIOContainer'
 import SampleReduxSocketIOContainer from '../sample/sample-redux-socketio/SampleReduxSocketIOContainer'
+import SampleReactLoadableRouterContainer from '../sample/sample-react-loadable-router/SampleReactLoadableRouterContainer'
 
 export default class FoobarContainer extends Component {
     handleClick() {
@@ -17,6 +18,7 @@ export default class FoobarContainer extends Component {
                 <button onClick={this.handleClick}>Click me!</button>
                 {/* <SampleSocketIO /> */}
                 <SampleReduxSocketIOContainer />
+                <SampleReactLoadableRouterContainer />
             </div>
         )
     }
