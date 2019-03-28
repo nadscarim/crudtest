8114781ae08ac52f9a688944e7aa18dde5d97990
jcmlumacad1 <jcmlumacad@tmj.ph>
2019-03-28 17:04:19 +0800

    organized chunks

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,17 @@ const webpack = require('webpack')
 const path = require('path')
 
 module.exports = {
-
     resolve: {
         alias: {
             'logger': path.resolve(__dirname, './logger')  // <-- When you build or restart dev-server, you'll get an error if the path to your logger.js file is incorrect.
         }
     },
-
     plugins: [
         new webpack.ProvidePlugin({
             'logger': 'logger'
         })
-    ]
-
+    ],
+    output: {
+        chunkFilename: 'dist/chunks/[name].[chunkhash].js'
+    }
 }
